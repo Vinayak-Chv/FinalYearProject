9d966ace0a7d88ab35e042b27aa5f83a45a9a7b4
Vinayak-Chv <bcavinayakchavan@gmail.com>
2026-02-21 11:49:03 +0530

    Added the underline to the Product card after hover

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProductCard = ({ product }) => {
     return (
-        <div className='bg-white rounded-lg shadow-md overflow-hidden'>
+        <div className='bg-white rounded-lg shadow-md overflow-hidden cursor-pointer'>
             <img src={product.images[0]} alt={product.title} className='w-full h-88 object-cover' />
             <div className='p-4'>
-                <h3 className='font-semibold'>{product.title}</h3>
+                <h3 className='font-semibold hover:underline'>{product.title}</h3>
                 <p className='text-primary font-bold mt-2'>₹{product.price}</p>
             </div>
         </div>
